e470d9a4d65f51d100c01ea6f560f0a8af4f40b6
Oleg Stotsky <ostotsky@gmail.com>
2018-09-18 17:42:56 +1000

    add precommit hooks; add eslint

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+let document;
+const root = document.getElementById('app');
+ReactDOM.render(<App />, root);
