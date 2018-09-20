a1acf28499c48f92daea07258feaa25b5b026b93
Oleg Stotsky <ostotsky@gmail.com>
2018-09-21 01:50:08 +1000

    add redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import {
+  devToolsEnhancer,
+  composeWithDevTools
+} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import { userReducer } from './user';
 
 export default () => {
+  const composeEnhancers = composeWithDevTools({});
   const store = createStore(
     combineReducers({
       user: userReducer
-    })
+    }),
+    composeEnhancers(applyMiddleware(thunk))
   );
 
   return store;
